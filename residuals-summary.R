3d6b699d10c46f108641ef8b9a2fb974509dd15f
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-01-15 12:55:56 -0800

    Stratified plots by timepoint & cleaned code

--- residuals-summary.R
+++ residuals-summary.R
@@ -11,19 +11,47 @@ library(ggplot2)
 # generated methylation predictions
 predictions <- read.csv('data-processed/methylation-predictions.csv')
 
+# sample information (contains ACEs score)
+sample <- read.csv('data-processed/pearls-acesmatchingbysexage.csv')
+
 # data wrangling
 ################################################################################
+# standardize format of clock names
+names(predictions) <- janitor::make_clean_names(names(predictions))
+
+# names of chronological age predictors
+chrono_age <- c('horvath_age', 
+                'horvath2', 
+                'hannum_age', 
+                'epigenetic_age_zhang', 
+                'c_age', 
+                'ped_be'
+                )
+
+# clean up sample info for joining with methylation predictions 
+sample_long <- sample %>%
+  tidyr::pivot_longer(cols = c(T2_specimenid, T5_specimenid),
+               names_pattern = '(.*)_(.*)',
+               names_to = c('timepoint', '.value')
+  ) %>%
+  mutate(specimenid = as.character(specimenid)) %>%
+  dplyr::rename(pearls_id = subjectid)
+
 # join methylation predictions (& residuals) with sample information
 residuals <- predictions %>%
-  mutate(specimenid = str_remove(sample_id,'^0+' # leading zeros
-  ),
-  specimenid = str_remove(specimenid,'..$' # last two characters
-  )
-  ) %>%
+  # create specimenid var to join with sample info downstream
+  mutate(specimenid = stringr::str_remove(sample_id,'^0+' # leading zeros
+                                          ),
+         specimenid = stringr::str_remove(specimenid,'..$' # last two characters
+                                          )
+         ) %>%
+  # keep only predictions for chronological age & sample info
   select(specimenid, age, imp_method, tissue, contains(chrono_age)) %>%
+  # keep only residuals of chronological age predictions & sample info
   select(specimenid, age, imp_method, tissue, contains('resid')) %>%
   # add in participant id info to match with demo data
   full_join(., sample_long, by = 'specimenid') %>%
+  # keep only predictions generated using KNN for missing CpG imputation
   filter(imp_method == 'knn') %>%
   # create categorical ACEs var (no ACEs/high ACEs) from ACE score at BASELINE
   mutate(aces = case_when(aces_baseline == 0 ~ "no",
@@ -54,6 +82,18 @@ resid_long %>%
              )
          ) +
   geom_boxplot() +
-  facet_wrap(~clock, nrow = 1)
+  facet_grid(clock ~ timepoint) +
+  ggtitle('Age acceleration residuals in blood samples')
   
 # buccal plot
+resid_long %>%
+  filter(clock == 'horvath2' | clock == 'ped_be_') %>%
+  filter(tissue == 'buccal') %>%
+  ggplot(aes(x = aces,# stratify on ACEs status
+             y = resid,
+             color = clock
+  )
+  ) +
+  geom_boxplot() +
+  facet_grid(clock ~ timepoint) +
+  ggtitle('Age acceleration residuals in buccal samples')
