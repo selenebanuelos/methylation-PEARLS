cc41d3182732f03085e1faa16d1a1e3591c5eeac
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-01-16 18:17:20 -0800

    Filtered for samples that passed qc
    Edited tables to only display stats for samples that passed DNAm QC. Also created separate tables for blood and buccal samples.

--- participant-descriptive-stats.R
+++ participant-descriptive-stats.R
@@ -17,7 +17,12 @@ age <- read.csv('data-raw/pearls_data_LauraDiaz_2025_11_20.csv')
 # data on caregiver education at baseline
 caregiver_edu <- read.csv('data-raw/pearls_dataset_2022-07-08.csv') %>%
   filter(visitnum == 1) %>%
-  select(pearls_id, caregiver_edu_4groups) 
+  select(pearls_id, caregiver_edu_4groups)
+
+# samples that passed qc
+passed_qc <- read.csv('data-processed/samples-passed-qc.csv') %>%
+  select(specimenid, passed_qc, tissue) %>%
+  mutate(specimenid = as.character(specimenid))
   
 # data wrangling
 ################################################################################
@@ -49,14 +54,16 @@ sample_long <- sample %>%
 # start with specimend id, sex, ACEs at baseline
 characteristics <- select(sample_long, !age_baseline) %>%
   # add in chronological age at each timepoint
-  left_join(.,
-            age_long,
+  left_join(age_long,
             by = c('pearls_id', 'timepoint')
             ) %>%
   # add in caregiver education at baseline
-  left_join(.,
-            caregiver_edu,
+  left_join(caregiver_edu,
             by = 'pearls_id'
+            ) %>%
+  # add in indicator of sample passing QC
+  left_join(passed_qc,
+            by = 'specimenid'
             )
 # will need to add in cell type eventually? but that might take some extra work
 # will need to review email from Kobor lab b/c I think they mentioned that cell
@@ -66,10 +73,31 @@ characteristics <- select(sample_long, !age_baseline) %>%
 
 # data visualization
 ################################################################################
-# create descriptive statistics table (table 1), stratified by adversity status
-table1(
+# create descriptive statistics tables (table 1), stratified by adversity status
+# table for blood samples
+stats_blood <- table1(
   # display the following characteristics, stratified on ACEs status
   ~ factor(sex) + age + caregiver_edu_4groups | aces_cat, 
-  data = characteristics
+  # specify which participants to include in table
+  data = characteristics %>%
+    # keep only blood data
+    filter(tissue == 'blood') %>%
+    # only include participants that passed QC
+    filter(passed_qc == 1),
+  caption = 'Descriptive statistics for blood samples'
   )
-# need to edit because 2 participants were removed due to failing QC of DNAme data
+
+# table for buccal samples
+stats_buccal <- table1(
+  # display the following characteristics, stratified on ACEs status
+  ~ factor(sex) + age + caregiver_edu_4groups | aces_cat, 
+  # specify which participants to include in table
+  data = characteristics %>%
+    # keep only blood data
+    filter(tissue == 'buccal') %>%
+    # only include participants that passed QC
+    filter(passed_qc == 1),
+  caption = 'Descriptive statistics for buccal samples'
+)
+
+# output
