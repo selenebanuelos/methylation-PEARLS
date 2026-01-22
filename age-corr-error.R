8440832e36da3300911f02d32501d6eeb139b523
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-01-22 15:23:17 -0800

    Created scatter plots showing DNAm v. Chrono age

--- age-corr-error.R
+++ age-corr-error.R
@@ -8,8 +8,67 @@ library(dplyr)
 library(ggplot2)
 
 # import data
+################################################################################
 DNAm_age <- read.csv('data-processed/dnam-age-sample-info.csv')
 
 # data wrangling
+################################################################################
+# clean up DNAm and chrono age data for plotting
+ages_long <- DNAm_age %>%
+  select(horvath2, ped_be, age, tissue, aces_baseline, timepoint) %>%
+  # make data longer for plotting downstream
+  tidyr::pivot_longer(
+    cols = c(horvath2, ped_be),
+    names_to = 'clock',
+    values_to = 'dnam_age'
+  ) %>%
+  # create categorical ACEs variable
+  mutate(aces = case_when(aces_baseline == 0 ~ 'no',
+                          aces_baseline >= 5 ~ 'high'
+                          )
+         )
 
 # data visualization
+################################################################################
+# create scatter plots with regression line to display correlation between
+# epigenetic age and chronological age across both timepoints for each tissue
+
+# blood samples
+ages_long %>%
+  # only plot data from blood samples
+  filter(tissue == 'blood') %>%
+  ggplot(aes(x = age,
+             y = dnam_age
+             )
+         ) +
+  # scatterplot showing relationship between DNAm age and chrono age
+  geom_point(
+    aes(color = aces) # color code points by ACEs status
+    ) +
+  # add regression line to show correlation
+  stat_smooth(method = 'lm',
+              formula = y ~ x,
+              geom = 'smooth'
+              ) +
+  # stratify plot by epigenetic clock
+  facet_wrap(~clock)
+
+# buccal samples
+ages_long %>%
+  # only plot data from blood samples
+  filter(tissue == 'buccal') %>%
+  ggplot(aes(x = age,
+             y = dnam_age
+  )
+  ) +
+  # scatterplot showing relationship between DNAm age and chrono age
+  geom_point(
+    aes(color = aces) # color code points by ACEs status
+  ) +
+  # add regression line to show correlation
+  stat_smooth(method = 'lm',
+              formula = y ~ x,
+              geom = 'smooth'
+  ) +
+  # stratify plot by epigenetic clock
+  facet_wrap(~clock)
