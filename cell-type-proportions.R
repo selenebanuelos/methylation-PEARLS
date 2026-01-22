0dad7de84b71940bb9591c0ae22963a3e206b4d4
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-01-22 11:22:41 -0800

    Created boxplots to visualize cell type proportions
    Boxplots grouped by exposure category, display distributions of cell type proportions in sample

--- cell-type-proportions.R
+++ cell-type-proportions.R
@@ -4,6 +4,8 @@
 
 # setup
 library(dplyr)
+library(tidyr)
+library(ggplot2)
 options(scipen = 999) # disable scientific notation
 
 # import data
@@ -20,19 +22,80 @@ buccal_cells <- readRDS('data-raw/Final_SampleInfo_Buccal_n38.rds') %>%
   mutate(other = 1 - rowSums(select(., !specimenid)))
 
 # sample information
-sample <- read.csv('data-processed/pearls-acesmatchingbysexage.csv')
+sample <- read.csv('data-processed/pearls-acesmatchingbysexage.csv') %>%
+  # transform to long format to get separate row for each timepoint
+  pivot_longer(cols = c(T2_specimenid, T5_specimenid),
+                      names_pattern = 'T(.)_(.*)', # remove 'T' from timepoint
+                      names_to = c('timepoint', '.value')
+                      ) %>%
+  # create categorical ACEs variable (no = 0, high >= 5)
+  mutate(aces = case_when(aces_baseline == 0 ~ 'no',
+                          aces_baseline >= 5 ~ 'high')
+  )
 
 # data wrangling
 ################################################################################
-# combine cell type proportions and sample timepoint/tissue information 
+# combine cell type proportions and sample timepoint/ACEs information 
+blood <- sample %>%
+  select(specimenid, timepoint, aces) %>%
+  # mutate specimenid to factor for joining downstream
+  mutate(specimenid = as.factor(specimenid)) %>%
+  # join sample information with cell type proportions
+  right_join(blood_cells,
+             by = 'specimenid') %>%
+  # make data longer for plotting
+  pivot_longer(
+    cols = Bas:other,
+    names_to = 'cell_type',
+    values_to = 'proportion'
+  )
+
+buccal <- sample %>%
+  select(specimenid, timepoint, aces) %>%
+  mutate(specimenid = as.factor(specimenid)) %>%
+  right_join(buccal_cells,
+             by = 'specimenid') %>%
+  pivot_longer(
+    cols = Epi:other,
+    names_to = 'cell_type',
+    values_to = 'proportion'
+  )
 
 # data visualization
 ################################################################################
-# bar plots
+# bar plots grouped by ACEs status
 # blood samples timepoint 2
+blood %>%
+  filter(timepoint == '2') %>%
+  ggplot(aes(x = cell_type,
+             y = proportion,
+             fill = aces)) +
+  geom_boxplot() + 
+  ggtitle('Blood samples T2')
 
 # buccal samples timepoint 2
+buccal %>%
+  filter(timepoint == '2') %>%
+  ggplot(aes(x = cell_type,
+             y = proportion,
+             fill = aces)) +
+  geom_boxplot() +
+  ggtitle('Buccal samples T2')
 
 # blood samples timepoint 5
+blood %>%
+  filter(timepoint == '5') %>%
+  ggplot(aes(x = cell_type,
+             y = proportion,
+             fill = aces)) +
+  geom_boxplot() +
+  ggtitle('Blood samples T5')
 
 # buccal samples timepoint 5
+buccal %>%
+  filter(timepoint == '5') %>%
+  ggplot(aes(x = cell_type,
+             y = proportion,
+             fill = aces)) +
+  geom_boxplot() +
+  ggtitle('Buccal samples T5')
