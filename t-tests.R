f6b9b07d04cf59ccdee9c21683a85a6547b083b0
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-03-25 15:20:23 -0700

    Changed measure of time from timepoint to chrono age

---
+++ t-tests.R
@@ -0,0 +1,5 @@
+### Author: Selene Banuelos
+### Date: 3/25/2026
+### Description: Run t-tests to compare average epigenetic age deviation between
+### no PEARLS and high PEARLS groups at baseline, follow-up, and difference
+### between baseline and follow-up
