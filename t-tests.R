1dd0464b0c5976661cfa11d7a1631f358d6f8439
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-03-26 11:27:17 -0700

    Delete t-tests.R

--- t-tests.R
+++
@@ -1,5 +0,0 @@
-### Author: Selene Banuelos
-### Date: 3/25/2026
-### Description: Run t-tests to compare average epigenetic age deviation between
-### no PEARLS and high PEARLS groups at baseline, follow-up, and difference
-### between baseline and follow-up
