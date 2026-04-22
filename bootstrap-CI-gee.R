e9cd2a8a8f4a4dd3ed8c283d90c1f8b9989f3e90
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-04-22 16:34:23 -0700

    Bootstrap 95% CIs for coefficient estimates

---
+++ bootstrap-CI-gee.R
@@ -0,0 +1,163 @@
+### Author: Selene Banuelos
+### Date: 4/5/2025
+### Description: Bootstrap 95% confidence interval for regression coefficient
+### from marginal model (GEE)
+
+# setup
+library(dplyr)
+library(tidyr)
+library(gee)
+
+# import data 
+################################################################################
+# analysis ready dataset
+data <- read.csv('data-processed/analysis-ready-dataset.csv')
+
+# data wrangling
+################################################################################
+analysis_vars <- c('pearls_id', 
+                   'income_FPL_100', 
+                   'horvath2', 
+                   'ped_be', 
+                   'age', 
+                   'tissue', 
+                   'sex', 
+                   'timepoint', 
+                   'pearls')
+
+# factor variables
+# pearls ID: needs to be factored to use with gee()
+data$pearls_id <- factor(data$pearls_id)
+
+# timepoint
+data$timepoint <- factor(data$timepoint,
+                          levels = c(2, 5)) # reference: 2
+# PEARLS status
+data$pearls <- factor(data$pearls,
+                       levels = c('no', 'high')) # reference: no
+# sex
+data$sex <- factor(data$sex, 
+                    levels = c('female', 'male')) # reference: female
+
+# household income below 100% federal poverty level for household of 4 (<25k)
+data$income_FPL_100 <- factor(data$income_FPL_100,
+                               levels = c('no', 'yes')) # reference: no
+
+# make data wider so we can do cluster-based bootstrap sampling (sample both
+# timepoints per individual)
+wide_data <- select(data, all_of(analysis_vars)) %>%
+  pivot_wider(
+    id_cols = c(pearls_id, tissue), 
+    names_from = timepoint,
+    values_from = c(income_FPL_100, horvath2, ped_be, age, sex, pearls),
+    names_glue = "{.value}_{timepoint}"
+  )
+    
+
+# bootstrapping setup
+################################################################################
+# set seed for reproducibiility
+set.seed(123)
+
+# specify number of bootstrap samples
+nb <- 10000
+
+# initial model fit to get names of coefficient estimates (tissue type doesn't matter)
+initial <- gee(horvath2 ~ age*pearls + pearls + age + sex + income_FPL_100,
+              id = pearls_id,
+              data = filter(data, tissue == 'blood'),
+              family = 'gaussian',
+              corstr = 'exchangeable')
+
+# specify number of columns in bootstrap coef matrix (# of covariates + 1 for error codes)
+p <- length(coef(initial)) + 1
+
+# create empty matrix to save coefficients from each bootstrap sample
+coef_mat <- matrix(0, nb, p)
+
+# need to name matrix with variable names corresponding to coefficients
+colnames(coef_mat) <- c(names(initial$coefficients), 'error_code')
+
+# split data into no/high adversity datasets to do stratified boostrap sampling
+################################################################################
+# sample stratified for no PEARLS participants
+blood_no <- wide_data %>%
+  # sample from participants with PEARLS = 0
+  filter(tissue == 'blood',
+         pearls_2 == 'no' | pearls_5 == 'no')
+
+# sample stratified for high PEARLS participants
+blood_high <- wide_data %>%
+  filter(tissue == 'blood',
+         pearls_2 == 'high' | pearls_5 == 'high')
+
+# sample stratified for no PEARLS participants
+buccal_no <- wide_data %>%
+  # sample from participants with PEARLS = 0
+  filter(tissue == 'buccal',
+         pearls_2 == 'no' | pearls_5 == 'no')
+
+# sample stratified for high PEARLS participants
+buccal_high <- wide_data %>%
+  filter(tissue == 'buccal',
+         pearls_2 == 'high' | pearls_5 == 'high')
+
+# create boostrapping function that outputs matrix of coefficient estimates
+################################################################################
+boot_sample <- function(i, no_df, high_df) {
+
+  # take sample from no PEARLS participants
+  boot_no <- no_df %>%
+    # randomly sample clusters (individuals) with replacement
+    slice_sample(n = 10, replace = TRUE) %>%
+    # assign new IDs to individuals (since old IDs may be repeated)
+    mutate(new_id = 1:10) %>%
+    # make data longer, creating separate rows for each timepoint per individual
+    pivot_longer(
+      cols = !c(pearls_id, new_id, tissue),
+      names_to = c(".value", "timepoint"),
+      values_to = "timepoint",
+      names_pattern = "(.*)_(.*)"
+    )
+  print(head(boot_no))
+  # take sample from high PEARLS participants (same as above)
+  boot_high <- high_df %>%
+    slice_sample(n = 10, replace = TRUE) %>%
+    # make new IDs different from those for no PEARLS participants
+    mutate(new_id = 11:20) %>%
+    pivot_longer(
+      cols = !c(pearls_id, new_id, tissue),
+      names_to = c(".value", "timepoint"),
+      values_to = "timepoint",
+      names_pattern = "(.*)_(.*)"
+    )
+  print(head(boot_high))
+  # combine samples from no PEARLS and high PEARLS groups
+  boot_all <- bind_rows(boot_no, boot_high)
+  
+  # fit linear model using generalized estimating equations
+  lm_gee <- tryCatch(
+    gee(
+      horvath2 ~ age*pearls + pearls + age + sex + income_FPL_100,
+      id = new_id, # make sure to use newly assigned IDs
+      data = boot_all,
+      family = 'gaussian',
+      corstr = 'exchangeable',
+      silent = TRUE
+      ),
+    error = function(e) NULL
+  )
+  
+  # save coefficient of interest and any error codes
+  if (is.null(lm_gee)) return(rep(NA_real_, p))
+  c(lm_gee$coefficients, lm_gee$error)
+  
+}
+
+# take nb bootstrap samples and save coefficients in matrix
+blood_mat <- coef_mat[,] <- do.call(rbind, lapply(seq_len(nb), boot_sample, blood_no, blood_high))
+buccal_mat <- coef_mat[,] <- do.call(rbind, lapply(seq_len(nb), boot_sample, buccal_no, buccal_high))
+
+# save matrices
+save(blood_mat, buccal_mat, file = "data-processed/bootstrap_estimates.RData")
+
---
+++ permutation-tests-sex.R
@@ -0,0 +1,218 @@
+### Author: Selene Banuelos adapted code by Mike Marin
+### Date: 4/22/2025
+### Description: Permutation testing to test for significant differences in 
+### average epigenetic age deviation between sex
+
+# setup
+library(dplyr)
+library(tidyr)
+options(scipen = 999)
+
+# import data 
+################################################################################
+# epigenetic age deviation (EAD) residuals
+ead <- read.csv('data-processed/methylation-predictions.csv')
+
+# import processed sample info
+blood_info <- readRDS('data-raw/Final_SampleInfo_Blood_n39.rds')
+buccal_info <- readRDS('data-raw/Final_SampleInfo_Buccal_n38.rds')
+
+# participant ages at each visit
+ages <- read.csv('data-raw/pearls_data_LauraDiaz_2025_11_20.csv') %>%
+  select(pearls_id, collectionage_t2, collectionage_t5) %>%
+  rename(subjectid = pearls_id)
+
+# data wrangling 
+################################################################################
+# sample information variables
+info_vars <- c('subjectid', 'Timepoint', 'Tissue', 'sex_.0.F.', 'Sample_Name')
+
+# combine sample information for blood and buccal samples
+sample_info <- rbind(
+  select(blood_info, info_vars),
+  select(buccal_info, info_vars)
+) %>%
+  # rename sample ID for downstream joining
+  rename(SampleID = Sample_Name)
+
+# join aar data with sample info and clean up for analysis
+combined <- ead %>%
+  # keep only EAD residuals
+  select(SampleID, Horvath2Resid, PedBEResid) %>%
+  # join residuals with sample information and PEARLS score
+  full_join(sample_info, by = 'SampleID') %>%
+  # create no PEARLS/high PEARLS groups from PEARLS score
+  mutate(sex = case_when(sex_.0.F. == 0 ~ 'female',
+                         sex_.0.F. == 1 ~ 'male'))
+
+# calculate difference in EAD and EAD trajectory
+ead_diff <- combined %>%
+  pivot_wider(id_cols = c(subjectid, Tissue, sex),
+              names_from = Timepoint,
+              values_from = c(Horvath2Resid, PedBEResid)
+  ) %>%
+  # add in subject age at each visit
+  left_join(ages, by = 'subjectid') %>%
+  # calculate difference in EAD
+  mutate(horvath2_diff = (Horvath2Resid_T5 - Horvath2Resid_T2),
+         pedbe_diff = (PedBEResid_T5 - PedBEResid_T2)
+  ) %>%
+  # calculate time between baseline and follow-up visits
+  mutate(time_between = collectionage_t5 - collectionage_t2) %>%
+  # calculate EAD trajectory (follow-up EAD - baseline EAD)/time between visits
+  mutate(horvath2_trajectory = horvath2_diff/time_between,
+         pedbe_trajectory = pedbe_diff/time_between)
+
+# create function that performs permutation test
+################################################################################
+permutation_test <- function(df, # (dataframe) data
+                             response, # (string) response variable to shuffle
+                             p_n = 100000 # number of permutation samples to take
+                             ){
+  
+  # generate permutation samples ###############################################
+  # the number of observations to sample (same size as original sample)
+  n <- nrow(df)
+  
+  # number of permutation samples to take
+  p <- 100000 
+  
+  # vector of the variable we're shuffling/sampling from (response variable)
+  # could shuffle the labels (PEARLS group) instead
+  var <- pull(df, response)
+  
+  # initialize a matrix (with all zeros) to store the permutation data
+  perm_samples <- matrix(0, nrow = n, ncol = p_n) # each col is a permutation sample 
+
+  # generate p_n permutation samples
+  for (i in 1:p_n){
+    
+    # use sample() to take samples without replacement from original dataset
+    perm_samples[,i] <- sample(var, size = n, replace = FALSE)
+    
+  }
+
+  # calculate test stats for each permutation sample ###########################
+  # initialize vector to store all of the test-stats
+  perm_test_stats <- rep(0, p_n) # vector same length as number of permutation samples
+  
+  # loop through and calculate the test statistics
+  for (i in 1:p_n){
+    
+    # calculate observed test-stat: difference in mean EAD between sex
+    perm_test_stats[i] <- mean(perm_samples[df$sex == 'male', i]) -
+      mean(perm_samples[df$sex == 'female', i])
+    # mean EAD in male participants - mean EAD in female participants
+  }
+
+  # calculate permutation p-value ##############################################
+  # what is the probability of getting the observed test statistic or more 
+  # extreme value if the null hypothesis of no difference is true?
+  
+  # get observed mean EAD in female participants 
+  mean_female <- filter(df, sex == 'female') %>%
+    pull(response) %>%
+    mean()
+  
+  # get observed mean EAD in male participants
+  mean_male <- filter(df, sex == 'male') %>%
+    pull(response) %>%
+    mean()
+  
+  # calculate observed test statistic 
+  obs_test_stat <- mean_male - mean_female
+  
+  # p-value = # of perm test-stats >= observed test stat/ total # perm test-stats
+  p_value <- sum(perm_test_stats >= obs_test_stat) / p_n
+  
+  return(sapply(c(obs_test_stat, p_value), round, digits = 2))
+  
+}
+
+# conduct permutation tests in blood samples
+################################################################################
+# Skin & Blood clock, baseline
+set.seed(123) # set seed for reproducibility
+bl_sb_baseline <- filter(combined, 
+                         Tissue == 'Blood',
+                         Timepoint == 'T2') %>%
+  permutation_test(., 'Horvath2Resid')
+
+# Skin & Blood clock, follow-up
+set.seed(123)
+bl_sb_followup <- filter(combined, 
+                         Tissue == 'Blood',
+                         Timepoint == 'T5') %>%
+  permutation_test(., 'Horvath2Resid')
+
+# SKin & Blood clock, EAD trajectory
+set.seed(123)
+bl_sb_traj <- filter(ead_diff,
+                     Tissue == 'Blood',
+                     !is.na(horvath2_trajectory)) %>%
+  permutation_test(., 'horvath2_trajectory')
+
+# PedBE clock, baseline
+set.seed(123) 
+bl_pbe_baseline <- filter(combined, 
+                         Tissue == 'Blood',
+                         Timepoint == 'T2') %>%
+  permutation_test(., 'PedBEResid')
+
+# PedBE clock, follow-up
+set.seed(123) 
+bl_pbe_followup <- filter(combined, 
+                          Tissue == 'Blood',
+                          Timepoint == 'T5') %>%
+  permutation_test(., 'PedBEResid')
+
+# PedBE clock, EAD trajectory
+set.seed(123)
+bl_pbe_traj <- filter(ead_diff,
+                     Tissue == 'Blood',
+                     !is.na(pedbe_trajectory)) %>%
+  permutation_test(., 'pedbe_trajectory')
+
+# conduct permutation tests in buccal samples
+################################################################################
+# Skin & Blood clock, baseline
+set.seed(123) # set seed for reproducibility
+bu_sb_baseline <- filter(combined, 
+                         Tissue == 'Buccal',
+                         Timepoint == 'T2') %>%
+  permutation_test(., 'Horvath2Resid')
+
+# Skin & Blood clock, follow-up
+set.seed(123)
+bu_sb_followup <- filter(combined, 
+                         Tissue == 'Buccal',
+                         Timepoint == 'T5') %>%
+  permutation_test(., 'Horvath2Resid')
+
+# Skin & Blood clock, EAD trajectory
+set.seed(123)
+bu_sb_traj <- filter(ead_diff,
+                      Tissue == 'Buccal',
+                      !is.na(horvath2_trajectory)) %>%
+  permutation_test(., 'horvath2_trajectory')
+
+# PedBE clock, baseline
+set.seed(123) 
+bu_pbe_baseline <- filter(combined, 
+                          Tissue == 'Buccal',
+                          Timepoint == 'T2') %>%
+  permutation_test(., 'PedBEResid')
+
+# PedBE clock, follow-up
+set.seed(123) 
+bu_pbe_followup <- filter(combined, 
+                          Tissue == 'Buccal',
+                          Timepoint == 'T5') %>%
+  permutation_test(., 'PedBEResid')
+
+# PedBE clock, EAD trajectory
+set.seed(123)
+bu_pbe_traj <- filter(ead_diff,
+                      Tissue == 'Buccal',
+                      !is.na(pedbe_trajectory)) %>%
+  permutation_test(., 'pedbe_trajectory')
