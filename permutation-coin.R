42920adeddb0ed46d69d6aaebae2b8da2556522d
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-05-22 12:03:33 -0700

    Wrapped oneway_test() in custom function
    Wrapped oneway_test() in custom function so I can apply it to multiple outcomes at once

--- permutation-coin.R
+++ permutation-coin.R
@@ -8,7 +8,6 @@
 library(dplyr)
 library(tidyr)
 library(coin)
-library(ggplot2)
 options(scipen = 999)
 
 # import data 
@@ -27,6 +26,9 @@ ages <- read.csv('data-raw/pearls_data_LauraDiaz_2025_11_20.csv') %>%
 
 # data wrangling 
 ################################################################################
+# list of biomarkers to do permutation testing on
+biomarkers <- list('HorvathAgeResid', 'Horvath2Resid', 'PedBEResid')
+
 # sample information variables
 info_vars <- c('subjectid', 
                'Timepoint', 
@@ -46,15 +48,17 @@ sample_info <- rbind(
 # join aar data with sample info and clean up for analysis
 combined <- ead %>%
   # keep only EAD residuals
-  select(SampleID, Horvath2Resid, PedBEResid) %>%
+  select(SampleID, HorvathAgeResid, Horvath2Resid, PedBEResid) %>%
   # join residuals with sample information and PEARLS score
   full_join(sample_info, by = 'SampleID') %>%
   # create no PEARLS/high PEARLS groups from PEARLS score
   mutate(pearls = case_when(aces_baseline == 0 ~ 'no',
                             aces_baseline >= 5 ~ 'high'),
-         pearls = as.factor(pearls)) %>%
-  # conducting permutation testing only in male participants 
-  filter(sex_.0.F. == 1) # 1 = male
+         pearls = as.factor(pearls)) 
+
+# stratify dataset by biological sex
+male <- filter(combined, sex_.0.F. == 1) # 1 = male
+female <- filter(combined, sex_.0.F. == 0) # 0 = female
 
 # calculate difference in EAD and EAD trajectory
 ead_diff <- combined %>%
@@ -74,43 +78,72 @@ ead_diff <- combined %>%
   mutate(horvath2_trajectory = horvath2_diff/time_between,
          pedbe_trajectory = pedbe_diff/time_between)
 
-blood_2 <- combined %>%
-  filter(Tissue == 'Blood',
-         Timepoint == 'T2')
-blood_5 <- combined %>%
-  filter(Tissue == 'Blood',
-         Timepoint == 'T5')
-buccal_2 <- combined %>%
-  filter(Tissue == 'Buccal',
-         Timepoint == 'T2')
-buccal_5 <- combined %>%
-  filter(Tissue == 'Buccal',
-         Timepoint == 'T5')
+# define functions
+################################################################################
+# function to do exact permutation testing using coin::oneway_test
+permutation_test <- function(outcome, # DNAm estimate name as string
+                             tissue,
+                             timepoint,
+                             data
+) {
+  # filter data for specified tissue and timepoint
+  df <- data %>%
+    filter(Tissue == tissue,
+           Timepoint == timepoint)
+  
+  # get outcome column as vector
+  y <- df[[outcome]]
+  
+  # get group column as vector
+  group <- df$pearls
+  
+  # exact permutation test
+  result <- coin::oneway_test(formula = y ~ group, 
+                              distribution = 'exact',
+                              alternative = 'two.sided')
+  
+  # get observed mean outcome in no PEARLS participants 
+  mean_no <- filter(df, pearls == 'no') %>%
+    pull(outcome) %>%
+    mean()
+  
+  # get observed mean outcome in high PEARLS participants
+  mean_high <- filter(df, pearls == 'high') %>%
+    pull(outcome) %>%
+    mean()
+  
+  # calculate observed test statistic 
+  obs_test_stat <- abs(mean_high - mean_no)
 
-all <- list(blood_2, blood_5, buccal_2, buccal_5)
+  
+  # return data frame with results
+  data.frame(tissue = tissue, # tissue type
+             clock = outcome,
+             timepoint = timepoint,
+             test_stat = obs_test_stat,
+             pvalue = pvalue(result), # get p-value from permutation test
+             data = deparse(substitute(data)) # dataframe name
+  )
+}
+
 # exact permutation testing
 ################################################################################
-horvath2 <- lapply(all,
-                   oneway_test(Horvath2Resid ~ pearls, 
-                               data = ., 
-                               distribution = approximate(nresample = 100000),
-                               alternative = 'greater'
-                               )
-                   )
+# do permutation tests for each biomarker in list
+blood_2 <- map_df(biomarkers, permutation_test, 'Blood', 'T2', combined)
+blood_5 <- map_df(biomarkers, permutation_test, 'Blood', 'T5', combined)
 
-# approximate (what i did before)
-# difference in mean outcome
-oneway_test(Horvath2Resid ~ pearls, 
-            data = test, 
-            ytrafo = function(Horvath2Resid) as.numeric(Horvath2Resid[[1]]), 
-            distribution = approximate(nresample = 100000),
-            alternative = 'greater'
-            )
-# difference in median outcomes
-oneway_test(Horvath2Resid ~ pearls, 
-            data = test, 
-            distribution = approximate(nresample = 100000),
-            alternative = 'greater'
-)
+buccal_2 <- map_df(biomarkers, permutation_test, 'Buccal', 'T2', combined)
+buccal_5 <- map_df(biomarkers, permutation_test, 'Buccal', 'T5', combined)
 
-oneway_test(PedBEResid ~ pearls, data = test, distribution = 'exact', alternative = 'greater')
+# sex-stratified testing 
+m_blood_2 <- map_df(biomarkers, permutation_test, 'Blood', 'T2', male)
+m_blood_5 <- map_df(biomarkers, permutation_test, 'Blood', 'T5', male)
+
+m_buccal_2 <- map_df(biomarkers, permutation_test, 'Buccal', 'T2', male)
+m_buccal_5 <- map_df(biomarkers, permutation_test, 'Buccal', 'T5', male)
+
+f_blood_2 <- map_df(biomarkers, permutation_test, 'Blood', 'T2', female)
+f_blood_5 <- map_df(biomarkers, permutation_test, 'Blood', 'T5', female)
+
+f_buccal_2 <- map_df(biomarkers, permutation_test, 'Buccal', 'T2', female)
+f_buccal_5 <- map_df(biomarkers, permutation_test, 'Buccal', 'T5', female)
